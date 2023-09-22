4ad8e4380744af48c3c90c0c20620927dc68e178
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-22 12:45:10 -0700

    Type_Sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of char: %zu bytes\n", sizeof(char));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
-	printf("Size of long: %zu bytes\n", sizeof(long));
-	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of a int: %zu bytes\n", sizeof(int));
+	printf("Size of an char: %zu bytes\n", sizeof(char));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+	printf("Size of a double: %zu bytes\n", sizeof(double));
+	printf("Size of a long: %zu bytes\n", sizeof(long));
+	printf("Size of a long long: %zu bytes\n", sizeof(long long));
 	return (0);
 }
