b757ff0928c6e5e489a96c4de99ee5836aee4071
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 16:49:28 -0700

    _Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * description - Prints _putchar
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char c[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8 ; i++)
+		{
+		_putchar(c[i]);
+		}
+
+	_putchar('\n');
+
+return (0);
+}
