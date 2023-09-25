e156d9c46f67c7fdc9a8dbdc4c29354c9fb57006
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 16:59:57 -0700

    Alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description - prints the alphabet
+ *
+ * Return: 0
+ **/
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,4 +3,8 @@
 
 int _putchar(char);
 
+void print_alphabet(void);
+
+
+
 #endif
