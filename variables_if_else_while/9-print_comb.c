9be14b1d05de9087fad5b1ab9bec6383af739d99
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 09:22:17 -0700

    Print_Combos

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *  * main - Prints the alphabetic
+ *   *
+ *    * Return: 0
+ *     **/
+int main(void)
+{
+	int n, c;
+
+	for (n = '0'; n <= '9'; n++)
+		for (c = '0'; c <= '9'; c++)
+		{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+		putchar(c + '0');
+		}
+	if (n != '9' || c != '9');
+	{
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+return (0);
+}
