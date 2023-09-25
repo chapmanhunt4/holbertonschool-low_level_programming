6951cc844b94ad1204adc6bab3e71dd4a9f59fc3
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 09:31:47 -0700

    Print_Combo

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,21 +7,18 @@
  **/
 int main(void)
 {
-	int n, c;
+	int n;
 
 	for (n = '0'; n <= '9'; n++)
-		for (c = '0'; c <= '9'; c++)
 		{
-		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
-		putchar(c + '0');
-		}
-	if (n != '9' || c != '9')
+		putchar(n);
+
+	if (n != '9')
 	{
 		putchar(',');
 		putchar(' ');
 	}
+		}
 
 	putchar('\n');
 
