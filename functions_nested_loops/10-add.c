d5cd135c2ffba1e197d72055c65e9e2f78c94bd7
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 20:09:28 -0700

    Add_Two

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - Where the program starts
+ *
+ * Return: A number that was added together
+ *
+ * @n: number to be added to m
+ *
+ * @m: number to be added to n
+ *
+ * Description: adds two numbers
+ **/
+
+int add(int n, int m)
+{
+	int j;
+
+	j = (n + m);
+
+	return (j);
+}
