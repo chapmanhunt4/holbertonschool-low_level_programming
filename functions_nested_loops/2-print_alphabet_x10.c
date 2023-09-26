d00d510bce2e4219fdb69679ba179d41430bdb5e
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-25 17:21:56 -0700

    Alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - entry point
+ *
+ * description - prints the alphabet
+ *
+ * Return: void
+ **/
+
+void print_alphabet(void)
+{
+	char c;
+	int n;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		while (n = 0; n < 10; n++)
+			putchar(c);
+	{
+	putchar('\n');
+	}
+}
