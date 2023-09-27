6e85680ac0d1d9c049eda26fbb92e4827dd1eeed
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-27 08:57:19 -0700

    More_Numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - entry point
+ *
+ * description - prints 0 to 14, 10 times
+ *
+ * Return: void
+ **/
+
+void more_numbers(void)
+{
+	char c = '0';
+	int i;
+	int n = 14;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		while (c <= 'n')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		c = '0';
+	}
+}
