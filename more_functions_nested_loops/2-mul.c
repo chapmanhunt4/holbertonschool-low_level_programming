dd2c57e49ec81411fcf9b8589ae9ac6f604930e4
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-27 08:07:16 -0700

    Multiply_A_

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int mul - Entry point
+ *
+ * Description - Multiple a to b
+ *
+ * Return 0 on proper execution
+ **/
+
+int mul(int a, int b)
+
+	{
+		return (a * b);
+	}
