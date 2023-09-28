9cff8888a2e8978a9d53243b48130a06555b5e6f
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-28 10:55:56 -0700

    FIZZBUZZ

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints FIZZ, BUZZ, and FIZZBUZZ
+ *
+ * Return: 0
+ **/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+return (0);
+}
