89962f8bcdb9b27944b7af894fb6a2395c92b621
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-09-28 10:23:05 -0700

    Diagonal_Line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ *
+ * @n: a variable that determines how many segments the program will have
+ *
+ * Return: void
+ **/
+
+void print_diagonal(void)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n')
+}
