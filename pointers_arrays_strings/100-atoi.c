a5367e76f4d8749ae37ea487a7606d2804f020fd
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-02 21:56:40 -0700

    ATOI

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _atoi - recreation of the atoi() standard function
+ * @str: the string to be converted to an integer
+ *
+ * Return: the integer produced from converting str
+ **/
+
+int _atoi(char *str)
+{
+	int i = 0;
+	int neg = 0;
+	int sign = 1;
+	int result = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+		{
+			neg += 1;
+		}
+
+		if ((str[i] < '0' || str[i] > '9') && (result > 0))
+		{
+			break;
+		}
+
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			if (result * 10 == 2147483640 && str[i] == '8')
+				result = (result * 10) + 7;
+			else
+				result = (result * 10) + (str[i]
+		}
+		i++;
+	}
+
+	if (neg % 2 == 1)
+	{
+		sign = -1;
+	}
+	if (result == 2147483647 && sign == -1)
+	{
+		result = result * sign;
+		return (result - 1);
+	}
+	return (result * sign);
+}
