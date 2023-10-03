537b9e4e8e6478e49ddfa595f45ddc916799bd8a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-02 21:59:18 -0700

    ATOI

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -31,7 +31,7 @@ int _atoi(char *str)
 			if (result * 10 == 2147483640 && str[i] == '8')
 				result = (result * 10) + 7;
 			else
-				result = (result * 10) + (str[i]
+				result = (result * 10) + (str[i];
 		}
 		i++;
 	}
