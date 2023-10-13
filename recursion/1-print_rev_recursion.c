9c4c79f6e6aa8c0a9853e5b9ad941bed6f2d77c4
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-13 11:22:30 -0700

    Rev_Recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: string to be printed
  *
+ * Return: exit if statement
  **/
 
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-	continue;
+		return;
 	}
 	else
 	{
