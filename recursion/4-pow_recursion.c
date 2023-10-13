904839f4ea580d19863abd19aab1c86e4e09f85c
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-13 12:25:17 -0700

    POW

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to be raised to the power of
+ * @y: power to be raised to
+ *
+ * Return: x to the power of y
+ **/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x _pow_recursion(y * (y + 1)));
+	}
+}
