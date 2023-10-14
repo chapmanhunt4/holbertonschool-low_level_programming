b442d3a5da360c34ad5a654c12a23edf42fd59b6
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-14 09:24:08 -0700

    Optimus

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,12 +13,31 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	else if (n / is_prime_number(n - 1))
+	else
+	{
+		return (find_prime(n, 2));
+	}
+}
+
+/**
+ * find_prime - calculation for recursive statement
+ * @x: base number
+ * @y: divisor
+ *
+ * Return: returns one if prime zero if not
+ **/
+
+int find_prime(int x, int y)
+{
+	if (x == y)
 	{
 		return (1);
 	}
-	else
+	else if (y % x == 0)
 	{
 		return (0);
 	}
-}
+	else
+	{
+		return (find_prime(x, y + 1));
+	}
