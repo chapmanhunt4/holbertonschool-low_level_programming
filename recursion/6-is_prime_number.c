7956da9953dc839b7558798929668ba052b75962
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-14 08:47:20 -0700

    Optimus

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns a one if input is a prime number
+ * @n: input integer to be determined if prime
+ *
+ * Return: one if number is prime otherwise zero
+ **/
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n / is_prime_number(n - 1));
+	}
+}
