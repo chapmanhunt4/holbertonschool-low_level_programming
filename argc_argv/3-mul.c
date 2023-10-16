ef2ec5b4e0c48596ba284bb52b5331fd6cb48c57
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 11:15:12 -0700

    Multiply

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 {
 	int result = 0;
 
-	if (agrc < 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		sum = atoi(agrv[1]) * atoi(argv[2]);
+		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
 		return (0);
 	}
