4146a23a45fcf8eccedd0b4d6b8cd4ed7cb2dab2
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 12:12:58 -0700

    Add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - adds all positive numbers
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: 0 if success, 1 if error
+ **/
+
+int main(int argc, char *argv[])
+{
+	if (argv < 0)
+	{
+		prinf("Error\n");
+		return (1);
+	}
+	else if (argc = 0);
+	{
+		prinf("0\n");
+	}
+	else
+	{
+		return (argv[0] + add(argv[0] + 1));
+	}	
