5f80baf4093054673ea4b18cc1b55844ef97028e
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 09:32:54 -0700

    Number

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Return: 0
+ **/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -5,5 +5,7 @@ int _putchar(char);
 
 int main(__attribute__((unused)) int argc, char *argv[]);
 
+int main(int argc, __attribute__((unused)) char *argv[]);
+
 
 #endif
