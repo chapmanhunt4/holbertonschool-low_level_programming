72bac0e5fffecabca63173f1982989fd385cf347
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 09:39:56 -0700

    Args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of function
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Return: 0
+ **/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[] - 1);
+
+	return (0);
+}
