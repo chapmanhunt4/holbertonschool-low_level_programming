a55bb43582887609cf74ca57141ec9b487227cae
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 20:22:41 -0700

    Add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - adds all positive numbers
@@ -9,21 +10,34 @@
  * Return: 0 if success, 1 if error
  **/
 
-int main(int argc, char *argv[])
-{
-	char *argv[] = argv
+int add_numbers(int argc, char *argv[], int index) {
+    if (index >= argc) {
+        return 0;
+    }
 
-	if (argv <=  1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (argc <= 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		return (argv[0] + add(argv[0] + 1));
-	}
-}	
+    for (int i = 0; argv[index][i] != '\0'; i++) {
+        if (!isdigit(argv[index][i])) {
+            return -1;
+        }
+    }
+
+    int num = atoi(argv[index]);
+    int next = add_numbers(argc, argv, index + 1);
+
+    if (next == -1) {
+        return -1;
+    }
+
+    return num + next;
+}
+
+int main(int argc, char *argv[]) {
+    int sum = add_numbers(argc, argv, 1);
+    if (sum == -1) {
+        printf("Error\n");
+        return 1;
+    } else {
+        printf("%d\n", sum);
+        return 0;
+    }
+}
