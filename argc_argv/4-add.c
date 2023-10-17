26b0259c347e0d27f82bdeae148f8635197bd061
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-16 20:25:30 -0700

    Add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,43 +1,32 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 /**
- * main - adds all positive numbers
- * @argc: number of arguments
- * @argv: arguments
+ * main - entry point, adds positive positive numbers
+ * @argc: argument count
+ * @argv: array of argument names
  *
- * Return: 0 if success, 1 if error
- **/
+ * Return: returns 0 on success
+ */
 
-int add_numbers(int argc, char *argv[], int index) {
-    if (index >= argc) {
-        return 0;
-    }
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
 
-    for (int i = 0; argv[index][i] != '\0'; i++) {
-        if (!isdigit(argv[index][i])) {
-            return -1;
-        }
-    }
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
 
-    int num = atoi(argv[index]);
-    int next = add_numbers(argc, argv, index + 1);
+		if (num <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-    if (next == -1) {
-        return -1;
-    }
-
-    return num + next;
-}
-
-int main(int argc, char *argv[]) {
-    int sum = add_numbers(argc, argv, 1);
-    if (sum == -1) {
-        printf("Error\n");
-        return 1;
-    } else {
-        printf("%d\n", sum);
-        return 0;
-    }
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
