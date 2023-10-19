a6e1583de1f4fc0250a3dfb97512ee29be41a41c
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-19 10:31:00 -0700

    String_Duplicate

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,7 +12,7 @@
 char *_strdup(char *str)
 {
 	char *strdup;
-	int i;
+	int i = 0;
 
 	if (!str)
 	{
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 
 	if (strdup)
 	{
-		for (i = 0; i <= strlen(str); i++)
+		for (; i <= strlen(str); i++)
 		{
 			strdup[i] = str[i];
 		}
