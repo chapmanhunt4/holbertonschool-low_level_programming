ac73426c3b358650369d731f56bc12a9353e801e
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-20 14:52:13 -0700

    Range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -23,14 +23,12 @@ int *array_range(int min, int max)
 	arr = malloc(max - (min - 1) * sizeof(int));
 
 	if (arr)
-
-	for (i = 0; min < max; i++)
-	{
-		arr[i] = min;
-		min++;
-	}
-	return (arr);
-	}
+		for (i = 0; min < max; i++)
+		{
+			arr[i] = min;
+			min++;
+		}
+		return (arr);
 	else
 	{
 		return (0);
