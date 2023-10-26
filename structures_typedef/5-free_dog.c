ffa36e0a82fd591b905533ddb14243bf8e2ac7bb
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 16:35:07 -0700

    FREE

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the doggo
+ * @d: doggo
+ **/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
