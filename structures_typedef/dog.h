f1db88ca73bbc50fb7de8d81a860de1f883c05e4
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 13:42:36 -0700

    DOG

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MAIN
 #define MAIN
 
+/**
+ * struct dog - struct with dogs name, age, and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ **/
+
 struct dog
 {
 	char *name;
