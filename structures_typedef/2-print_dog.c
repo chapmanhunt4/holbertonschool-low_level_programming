4e0b6f2361d4c7d8f3e8cb97a99e9f437b74b27b
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 14:10:10 -0700

    Print_Dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	char n = (nil)
+	int i;
+
+	if (d == NULL)
+	{
+		return
+	}
+	if (d)
+	{
+		for (i = 0; i < d; i++)
+		{
+			d[i] == NULL;
+			printf("%c", n);
+		}
+	}
+	if (name == NULL)
+	{
+		printf("Name: %c", n);
+	}
+}
