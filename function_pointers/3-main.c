9417fa7cce95a797f29c5b1590efb49a9705744f
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-27 15:20:54 -0700

    Calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2] == NULL) || (argv[2] != ops[1] && argv[2] != ops[2] && argv[2] != ops[3] && argv[2] != ops[4] && argv[2] != ops[5]))
+	if ((argv[2] == NULL) || (argv[2] != op[1] && argv[2] != op[2] && argv[2] != op[3] && argv[2] != op[4] && argv[2] != op[5]))
 	{
 		printf("Error\n");
 		exit(99);
