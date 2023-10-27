e71d715fc12adfb263da04b6915ed5143cd75c1c
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-27 14:11:58 -0700

    Calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit (100);
 	}
-	result = (*get_op_func(argv[2])), num1, num2;
+	result = ((*get_op_func(argv[2])), num1, num2);
 
 	printf("%d\n", result);
 	return (0);
