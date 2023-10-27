60860b5e190c40c347d75bae01b476dea45ee78e
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 17:08:17 -0700

    New_Dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,13 +14,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new = malloc(sizeof(dog_t));
+	new->name = malloc(strlen(name) + 1);
+	new->owner = malloc(strlen(owner) + 1);
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 
-	new->name = strdup(name);
+	new->name = strdup(name)		
 	if (new->name == NULL)
 	{
 		free(new);
