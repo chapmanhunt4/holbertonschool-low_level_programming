270947d707cea2123916cd39c776aed6608d2d2b
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 17:29:24 -0700

    New_Dog

---
+++ structures_typedef/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a duplicate string
+ * @str: string to be duplicated
+ *
+ * Return: pointer to duplicated string, or NULL
+ **/
+
+char *_strdup(char *str)
+{
+	char *strdup;
+	unsigned long int i;
+
+	if (!str)
+	{
+		return (0);
+	}
+
+	strdup = malloc(strlen(str) + 1);
+
+	if (strdup)
+	{
+		for (i = 0; i <= strlen(str); i++)
+		{
+			strdup[i] = str[i];
+		}
+		return (strdup);
+	}
+	free(strdup);
+	return (0);
+}
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "1-strdup.c"
 
 /**
  * new_dog - copies and creates new struct
@@ -14,22 +15,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new = malloc(sizeof(dog_t));
-	new->name = malloc(strlen(name) + 1);
-	new->owner = malloc(strlen(owner) + 1);
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 
-	new->name = strdup(name);
+	new->name = _strdup(name);
 	if (new->name == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	new->owner = strdup(owner);
+	new->owner = _strdup(owner);
 	if (new->owner == NULL)
 	{
 		free(new->name);
