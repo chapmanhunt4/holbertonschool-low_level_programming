014a0f339d3b2f8683fdfc9d8cf447cd54ff0be7
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-26 18:33:30 -0700

    Array_Size

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -15,11 +15,14 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action)
+	if (array)
 	{
-		for(i = 0; i < size; i++)
+		if (action)
 		{
-			action(array[i]);
+			for (i = 0; i < size; i++)
+			{
+				action(array[i]);
+			}
 		}
 	}
 }
