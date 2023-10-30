9a1e1f07dec56408c2a9b1a1efb8a527685f6569
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-10-30 13:58:55 -0700

    Numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator)
+		if (i > 0 && separator)
 		{
 			printf("%s", separator);
 		}
