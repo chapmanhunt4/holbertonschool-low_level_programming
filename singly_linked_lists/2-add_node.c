708eb848da72e3a1db79bae52dc2a7ffe0a663fb
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-08 14:36:01 -0800

    set new_node to strdup of str and return new_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,18 +12,15 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	char *string;
 	list_t *new_node = NULL;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	new_node->str = string;
+	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
-	free(new_node);
-	return (string);
+	return (new_node);
 }
