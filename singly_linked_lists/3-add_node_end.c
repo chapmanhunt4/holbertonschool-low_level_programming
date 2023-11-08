a644f826e18d30afd763585af45a3fd6d211eb15
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-08 15:42:23 -0800

    attempt to fix

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *ptr, *temp;
 	ptr = *head;
 
-	new_node = malloc(sizeof(list_t));
+	temp = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
