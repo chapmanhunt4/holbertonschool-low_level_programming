ceeafba4fc6857f274cbaec5be06e01b3defaf85
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-09 11:17:14 -0800

    created func to delete first node

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes head node of a list
+ * @head: pointer to first node in list
+ * Return: head nodes data, or zero if empty
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+	return (temp->n);
+}
