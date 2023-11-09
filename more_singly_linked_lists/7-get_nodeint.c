2cde0709700045667119df5f10134a0365771af0
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-09 14:22:03 -0800

    used i and derefrenced head

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -20,10 +20,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 
 
-	ptr = *head;
+	ptr = head;
 	while (ptr != NULL)
 	{
-		if (index)
+		if (i == index)
 		{
 			return (ptr);
 		}
