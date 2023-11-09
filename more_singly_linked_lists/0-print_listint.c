ace7759572e633f7e5467774a23c787d14cb27f5
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-08 17:28:10 -0800

    moved increment of size and pointer to node

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -14,9 +14,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->data);
 		size++;
+		printf("%d\n", h->data);
+		h = h->next;
 	}
-	h = h->next;
 	return (size);
 }
