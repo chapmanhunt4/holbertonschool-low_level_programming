b3d46684fb18cf5ef7080bf1e49fa8c5741b4e68
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-08 16:00:17 -0800

    attempt to fix seg fault

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,21 +16,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	ptr = *head;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new_node)
 	{
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = NULL;
+
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = new_node;
 		free(new_node);
-		return (NULL);
+		return (new_node);
 	}
-
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-	new_node->next = NULL;
-
-	while (ptr->next != NULL)
-	{
-		ptr = ptr->next;
-	}
-	ptr->next = new_node;
 	free(new_node);
-	return (new_node);
+	return (NULL);
 }
