3a2e44f93d17e9b17b728175c01a8db87eebdc74
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-11 10:25:54 -0800

    function to add node at index

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - inserts new node at given position
+ * @head: pointer to first node in list
+ * @idx: position at which node is added
+ * @n: data of node
+ * Return: address of new node, or NULL
+ **/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listunt_t *new_node;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = *head;
+	while (new_node != NULL)
+	{
+		if (i == idx)
+		{
+			new_node->n = n;
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
+	}
+}
