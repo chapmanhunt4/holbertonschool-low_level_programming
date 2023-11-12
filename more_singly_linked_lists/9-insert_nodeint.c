5861ce12419738375f2ec007e46702e2733a227a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-12 12:50:56 -0800

    changed syntax from ptr2->next = ptr2 to ptr->next = ptr2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 	}
 	ptr2->next = ptr->next;
-	ptr2->next = ptr2;
+	ptr->next = ptr2;
 	return (ptr2);
 }
