cc713a06eefdde733f862dfaa9c2835b2516383d
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-12 12:52:46 -0800

    moved i increment

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -37,8 +37,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
-		i++;
 		ptr = ptr->next;
+		i++;
 	}
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
