fca937d37e582805f95363b5b5a1dd8e2851bf4b
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-13 07:33:33 -0800

    made a function that returns value of a bit at index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_bit - returns value of a bit at index
+ * @n: bit given
+ * @index: place at which bit is retrieved
+ * Return: value of bit at index
+ **/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i, size;
+
+	size = sizeof(n) * 8;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		if (i == index)
+		{
+			return ((n >> i) & 1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+}
