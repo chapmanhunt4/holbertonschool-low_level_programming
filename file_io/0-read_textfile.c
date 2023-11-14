2987f02ab1b8333d09bcd9e92eac804e6a3e3872
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-13 20:39:56 -0800

    made a function that reads a text file and prints it to stdout

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of file
+ * @letters: characters to be read
+ * Return: number of letters read
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i, n, descriptor;
+	char *size;
+
+	size = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	descriptor = open(filename, O_RDONLY);
+	if (!descriptor)
+	{
+		close(descriptor);
+		return (0);
+	}
+
+	n = read(descriptor, size, letters);
+	if (n <= 0)
+	{
+		close(descriptor);
+		return (0);
+	}
+
+	for (i = 0; i < letters && i < n; i++)
+	{
+		write(STDOUT_FILENO, &size[i], 1);
+	}
+	close(descriptor);
+	return (n);
+}
