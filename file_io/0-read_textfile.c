14b91991b38a5c62f3f83e6b34eb05de81c50e38
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-13 20:45:06 -0800

    added proper headers

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to standard output
--- file_io/main.h
+++ file_io/main.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef MAIN
 #define MAIN
 
