a4a235de1bfaba74366f1aed4dda7aed040e78f5
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-15 09:46:35 -0800

    changed ptr to temp in function bc changed name of variable prev

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
-	ptr = malloc(sizeof(dlistint_t));
+	temp = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
 		return (NULL);
