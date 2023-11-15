b8c6baf14e463413fd6c7407084bd347f747c4c6
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-15 08:58:33 -0800

    made head pointer to prev equal to new_node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,11 +16,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	while (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = *head;
-		*head = new_node;
-	}
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	(*head)->prev = new_node;
+	*head = new_node;
 	return (new_node);
 }
