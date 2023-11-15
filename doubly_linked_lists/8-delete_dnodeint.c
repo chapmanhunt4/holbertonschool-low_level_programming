3d957519bd0d4e3c7d2ab22fad2588b9250d01da
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-15 15:09:51 -0800

    made function to free at index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - deletes node at a given index
+ * @head: pointer to first node in the list
+ * @index: point to delete at
+ * Return: 1 on success, -1 on fail
+ **/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *temp2;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	temp = *head;
+	while (i < idx - 1)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+	temp2 = temp->next;
+	if (temp2->next != NULL)
+	{
+		temp->next = temp2->next;
+		temp2->next->prev = temp;
+	}
+	else
+		temp->next = NULL;
+	free(temp);
+	return (1);
