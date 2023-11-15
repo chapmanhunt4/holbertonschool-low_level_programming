ee22d4dd48d0f6e8e89385c93af25919b69de97a
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-15 08:51:07 -0800

    changed to a while loop

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,11 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
+	while (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+	}
 	return (new_node);
 }
