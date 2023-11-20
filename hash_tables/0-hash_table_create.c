8d50601ae3e6057156cf145d844716a9d5e9abe2
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-20 08:58:45 -0800

    made it allocate for the sizeof hashnode pointers, not just hash nodes and fixed betty hopefully

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,20 +12,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *hash;
-
 	hash = malloc(sizeof(hash_table_t));
 
 	if (!hash)
+	{
 		return (NULL);
-
+	}
 	hash->size = size;
-	hash->array = malloc(hash->size * sizeof(hash_node_t));
-
+	hash->array = malloc(hash->size * sizeof(hash_node_t *));
 	if (!hash->array)
+	{
 		return (NULL);
-
+	}
 	for (i = 0; i < hash->size; i++)
+	{
 		hash->array[i] = NULL;
-
+	}
 	return (hash);
 }
