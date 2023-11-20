000053c0d6fe23b7257810c5818496033d62e31f
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-11-20 09:05:06 -0800

    had to make small change to file to get script change of file to be reconized

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- * Return: a pointer to hash table
+ * Return: a pointer to new hash table
  **/
 
 hash_table_t *hash_table_create(unsigned long int size)
