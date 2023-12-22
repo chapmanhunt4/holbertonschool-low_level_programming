1c71aed7c1992ba8f58445ab7988a64ae85ce2d1
chapmanhunt4 <chapmanhunt4@gmail.com>
2023-12-22 15:20:36 -0800

    made readme and header

---
+++ search_algorithms/README.md
@@ -0,0 +1,1 @@
+A project over search algorithms
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+
+int binary_search(int *array, size_t size, int value);
+
+#endif
